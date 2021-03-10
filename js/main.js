36ffbbf6d5e0bfcd708d7277fe244809c3b12392
Kristofer Whitfield <mrkjwhitfield@gmail.com>
2021-03-09 23:54:51 -0500

    Second

--- js/main.js
+++ js/main.js
@@ -2,3 +2,20 @@
 function numberToString(num) {
   return num.toString();
 }
+
+// What is the most asked question on CodeWars?
+//
+// Can someone explain /*...*/?
+//
+// You need to write a function detect that will check if a string starts with Can someone explain, case sensitive. Return true if so, false otherwise.
+
+function detect(comment) {
+  let str = comment.split(' ')
+  if(str.includes('Can',[0]) && str.includes('someone',[1]) && str.includes('explain',[2])){
+    return true
+    }
+      else {
+        return false
+      }
+
+}
