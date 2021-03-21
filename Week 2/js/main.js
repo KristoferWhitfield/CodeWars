6cd1dd65ad083f00899879ab5ae74d86fa12ea6e
Kristofer Whitfield <78818078+KristoferWhitfield@users.noreply.github.com>
2021-03-20 23:13:12 -0400

    next

--- Week 2/js/main.js
+++ Week 2/js/main.js
@@ -80,3 +80,9 @@ function makeNegative(num) {
       return  num * -1
     }
 }
+
+
+// This method, which is supposed to return the result of dividing its first
+// argument by its second, isn't always returning correct values. Fix it.
+
+const solve = (x, y) => x / y
