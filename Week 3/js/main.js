18310e707c1fd31727da3cc97998fb07c9d22d0f
Kristofer Whitfield <78818078+KristoferWhitfield@users.noreply.github.com>
2021-03-22 21:09:23 -0400

    test

--- Week 3/js/main.js
+++ Week 3/js/main.js
@@ -1,1 +1,13 @@
+function mergeArrays(a, b) {
 
+  console.log(a.concat(b).sort())
+
+}
+
+mergeArrays([1,3,5], [2,3,6])
+
+function lonelyInteger(arr){
+  console.log(parseInt(arr.filter((v, i) => arr.indexOf(v) === arr.lastIndexOf(v))))
+}
+
+lonelyInteger([1,1,2,2,4])
