0cec722e504041e2f55311b69e4db5517e9e2349
Kristofer Whitfield <78818078+KristoferWhitfield@users.noreply.github.com>
2021-04-06 23:27:03 -0400

    c

--- Week 5/js/main.js
+++ Week 5/js/main.js
@@ -2,12 +2,12 @@
 //  take one argument animal which corresponds to the animal encountered by frog.
 //   If this one is an alligator (case insensitive) return small otherwise return
 //   wide.
-function mouthSize(animal) {
-  // code here
-  if(animal === "alligator"){
-    return "small"
-  }
-  else {
-    return "wide"
-  }
-}
+// function mouthSize(animal) {
+//   // code here
+//   if(animal === "alligator"){
+//     return "small"
+//   }
+//   else {
+//     return "wide"
+//   }
+// }
