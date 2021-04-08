29df4f8fa729eecde02f71ec956b2245cc869025
Kristofer Whitfield <78818078+KristoferWhitfield@users.noreply.github.com>
2021-04-07 23:23:04 -0400

    n

--- Week 5/js/main.js
+++ Week 5/js/main.js
@@ -11,3 +11,8 @@
 //     return "wide"
 //   }
 // }
+// The code provided is supposed replace all the dots . in the
+// specified String str with dashes -
+var replaceDots = function(str) {
+  return str.replace('.', '-');
+}
