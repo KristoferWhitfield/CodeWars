34733e952a12187be343684bc214fe3ad69479c3
Kristofer Whitfield <78818078+KristoferWhitfield@users.noreply.github.com>
2021-04-08 22:52:20 -0400

    ui

--- Week 5/js/main.js
+++ Week 5/js/main.js
@@ -16,3 +16,8 @@
 var replaceDots = function(str) {
   return str.replace('.', '-');
 }
+// 
+// Implement a function named generateRange(min, max, step), which takes three
+//  arguments and generates a range of integers from min to max, with the step.
+//  The first integer is the minimum value, the second is the maximum of the range
+//   and the third is the step. (min < max)
