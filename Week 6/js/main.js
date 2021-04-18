551a1913292c37757424d28f0b54e7aafd92381a
Kristofer Whitfield <78818078+KristoferWhitfield@users.noreply.github.com>
2021-04-17 22:22:48 -0400

    e

--- Week 6/js/main.js
+++ Week 6/js/main.js
@@ -36,3 +36,17 @@
 function main(verb, noun){
   return verb + noun
 }
+
+// Alex just got a new hula hoop, he loves it but feels discouraged because
+// his little brother is better than him
+//
+// Write a program where Alex can input (n) how many times the hoop goes round and
+// it will return him an encouraging message :)
+function hoopCount (n) {
+   if(n >= 10){
+     return 'Great, now move on to tricks'
+   }
+  else{
+    return 'Keep at it until you get it'
+  }
+}
