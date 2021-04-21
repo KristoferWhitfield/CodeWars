33d9a07a11da4b717c1ddc4a416d1abc5d4e5c11
Kristofer Whitfield <78818078+KristoferWhitfield@users.noreply.github.com>
2021-04-20 22:30:45 -0400

    n

--- Week 7/js/main.js
+++ Week 7/js/main.js
@@ -1,2 +1,8 @@
-// here's a "3 for 2" (or "2+1" if you like) offer on mangoes. For a given quantity 
+// here's a "3 for 2" (or "2+1" if you like) offer on mangoes. For a given quantity
 // and price (per mango), calculate the total cost of the mangoes.
+
+// 
+// Write a function to get the first elements of asequence. Passing a parameter n
+// (default=1) will return the first n elements of the sequence.
+//
+// If n == 0 return an empty sequence []
