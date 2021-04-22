6288f51908e6b05ce08e611f7a7c70ebfdd2b827
Kristofer Whitfield <78818078+KristoferWhitfield@users.noreply.github.com>
2021-04-21 21:42:22 -0400

    +

--- Week 7/js/main.js
+++ Week 7/js/main.js
@@ -1,8 +1,20 @@
 // here's a "3 for 2" (or "2+1" if you like) offer on mangoes. For a given quantity
 // and price (per mango), calculate the total cost of the mangoes.
 
-// 
+//
 // Write a function to get the first elements of asequence. Passing a parameter n
 // (default=1) will return the first n elements of the sequence.
 //
 // If n == 0 return an empty sequence []
+
+// Your job is simple, if (x) squared is more than 1000, return 'It's hotter than
+// the sun!!', else, return 'Help yourself to a honeycomb Yorkie for the glovebox.'.
+
+function apple(x){
+  if(Math.pow(x, 2) > 1000){
+    return  "It's hotter than the sun!!"
+  }
+  else {
+    return 'Help yourself to a honeycomb Yorkie for the glovebox.'
+  }
+}
